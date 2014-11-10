608cc56413758053cbf009056205345d97c80dab
Vanilla Hsu <vanilla@fatpipi.com>
2014-11-10 16:24:33 +0800

    Fix build.

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"time"
 )
 
-func sendUdp(content string) {
-	udpAddr, err := net.ResolveUDPAddr("udp", cfg.Lcp.Collector)
+func sendUdp(addr, content string) {
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
 		log.Fatalf("xxx")
 	}
@@ -38,18 +38,18 @@ func genLogPath(format string, t time.Time) string {
 	return logf
 }
 
-func Logger(format string, v ...interface{}) {
+func Logger(logFileFormat, contentFormat string, v ...interface{}) {
 	var buffer bytes.Buffer
 
 	t := time.Now()
-	logf := genLogPath(*logFormat, t)
+	logFH := genLogPath(logFileFormat, t)
 
-	dirname := filepath.Dir(logf)
+	dirname := filepath.Dir(logFH)
 	if _, err := os.Stat(dirname); err != nil {
 		os.MkdirAll(dirname, 0644)
 	}
 
-	f, err := os.OpenFile(logf, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	f, err := os.OpenFile(logFH, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
@@ -57,7 +57,7 @@ func Logger(format string, v ...interface{}) {
 	buffer.WriteString("[")
 	buffer.WriteString(t.String())
 	buffer.WriteString("] ")
-	buffer.WriteString(format)
+	buffer.WriteString(contentFormat)
 
 	if len(v) > 0 {
 		fmt.Fprintf(f, buffer.String(), v...)
